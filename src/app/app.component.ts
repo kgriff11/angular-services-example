084239edb98b08e1b8bf7461ed03962224f3cd70
Kelso Griffin <kgriffin.c@xpanxion.com>
2022-06-17 11:26:54 -0500

    Added updates to my project for angular services using dependency injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Product } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[ProductService]
 })
 export class AppComponent {
   title = 'service-example';
+
+  products: Product[];
+
+  constructor(private productService: ProductService){
+    
+  }  
+
+  getProducts(){
+    this.products = this.productService.getProducts();
+  }
+
+
 }
---
+++ src/app/product.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Product } from './product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor() { }
+
+  public getProducts(){
+    let products: Product[];
+
+    products=[
+      new Product(1, 'Memory Card', 500),
+      new Product(2, 'SSD', 750),
+      new Product(3, 'Power Bank', 100)
+    ]
+
+    return products;
+  }
+}
